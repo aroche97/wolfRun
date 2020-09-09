5736221384b6d0ab9bdf7bb177c8b5be6e260c3c
aroche97 <60188915+aroche97@users.noreply.github.com>
2020-09-08 20:58:57 -0500

    scripts continued

---
+++ Scripts/Coins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Coins : MonoBehaviour {
+
+    public int coinValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+        Destroy(gameObject);
+    }
+  
+}
---
+++ Scripts/WolfJump.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+
+public class WolfJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    public float jumpForce;
+    private BoxCollider2D bc;
+    public LayerMask platform;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+
+    }
+    
+    // Update is called once per frame
+    
+    
+    void Update()
+    {
+        Jump();
+
+    } 
+
+    void Jump()
+    {
+        if (!isGrounded())
+        {
+            return;
+        } else {
+           // jump 
+        }
+        //if (isGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    bool isGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, platform);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
