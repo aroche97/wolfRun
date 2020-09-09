8e9eb70eec381e795c2fda27f6370bbbba927690
aroche97 <60188915+aroche97@users.noreply.github.com>
2020-09-08 20:55:54 -0500

    scripts

---
+++ Movement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    
+   
+   
+
+    // Start is called before the first frame update
+
+    Vector3 characterScale;
+    float characterScaleX;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        characterScale = transform.localScale;
+        characterScaleX = characterScale.x;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+}
+  
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -characterScaleX;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = characterScaleX;
+        }
+        transform.localScale = characterScale;
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = 
+            Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    }
+
---
+++ UiManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UiManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("wolfRunmoved");
+    }
+
+public void doExitGame()
+{
+    Application.Quit();
+}
+
+    public void levelComplete()
+    {
+        SceneManager.LoadScene("winMenu");
+    }
+}
